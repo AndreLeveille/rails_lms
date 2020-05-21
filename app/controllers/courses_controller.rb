447e2998e753e2803a441202af9737443ef53ff6
Andre Leveille <andre.joe.leveille@gmail.com>
2020-05-21 11:15:18 -0600

    basic controller

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,30 @@
+class CoursesController < ApplicationController
+  
+  def index
+    @courses = Course.all
+  end
+  def show
+  end
+  def new
+    @course = Course.new
+  end
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to @course
+    else
+      render :new
+    end
+  end
+  def destroy
+    @course.destroy
+    redirect_to courses_path
+  end
+  private
+    def set_course
+      @course = Course.find(params[:id])
+    end
+    def course_params
+      params.require(:course).permit(:name)
+    end
+end
